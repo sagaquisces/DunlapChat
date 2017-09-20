bf8ef332ef7767262de5c788e66666399b3adb78
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-20 15:04:52 -0700

    app will allow logout

--- app/src/main/java/com/example/guest/dunlapchat/MainActivity.java
+++ app/src/main/java/com/example/guest/dunlapchat/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.guest.dunlapchat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 //    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
@@ -17,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Dunlap Chat");
     }
 
     @Override
@@ -25,9 +33,37 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         if(currentUser == null){
-            Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
-            startActivity(startIntent);
-            finish();
+            sendToStart();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.main_logout_btn) {
+            FirebaseAuth.getInstance().signOut();
+            sendToStart();
+        }
+
+        return true;
+
+
+    }
+
+    public void sendToStart() {
+        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
     }
 }
