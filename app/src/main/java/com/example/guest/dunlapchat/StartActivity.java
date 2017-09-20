95a26602ba286b54e5a23b77b97b307d34c81a1b
Guest User <Guest@epicodus-50.local>
2017-09-20 11:26:27 -0700

    app will navigate to Start Activity if currentUser is null

---
+++ app/src/main/java/com/example/guest/dunlapchat/StartActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.dunlapchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
