0d18335784caf8844e42b3af9c753cb5356a9e78
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-20 16:34:01 -0700

    app will allow new user to create account and see a progress dialog that asks user to wait.

--- app/src/main/java/com/example/guest/dunlapchat/RegisterActivity.java
+++ app/src/main/java/com/example/guest/dunlapchat/RegisterActivity.java
@@ -1,11 +1,13 @@
 package com.example.guest.dunlapchat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +18,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import org.w3c.dom.Text;
-
 public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = RegisterActivity.class.getSimpleName();
 
@@ -27,6 +27,8 @@ public class RegisterActivity extends AppCompatActivity {
     private Button mCreateBtn;
 
     private Toolbar mToolbar;
+
+    private ProgressDialog mRegProgress;
 
     private FirebaseAuth mAuth;
 
@@ -40,11 +42,13 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Create Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mRegProgress = new ProgressDialog(this);
+
         mAuth = FirebaseAuth.getInstance();
 
-        mDisplayName = (TextInputLayout) findViewById(R.id.reg_display_name);
+        mDisplayName = (TextInputLayout) findViewById(R.id.login_email);
         mEmail = (TextInputLayout) findViewById(R.id.reg_email);
-        mPassword = (TextInputLayout) findViewById(R.id.reg_password);
+        mPassword = (TextInputLayout) findViewById(R.id.login_password);
         mCreateBtn = (Button) findViewById(R.id.reg_create_button);
 
         mCreateBtn.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +58,16 @@ public class RegisterActivity extends AppCompatActivity {
                 String email = mEmail.getEditText().getText().toString();
                 String password = mPassword.getEditText().getText().toString();
 
-                register_user(display_name, email, password);
+                if(!TextUtils.isEmpty(display_name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+                    mRegProgress.setTitle("Registering User");
+                    mRegProgress.setMessage("Please wait..");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+
+                    register_user(display_name, email, password);
+                }
+
+
             }
         });
 
@@ -71,9 +84,13 @@ public class RegisterActivity extends AppCompatActivity {
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
+                            mRegProgress.hide();
                             Toast.makeText(RegisterActivity.this, R.string.auth_failed,
                                     Toast.LENGTH_SHORT).show();
                         } else {
+
+                            mRegProgress.dismiss();
+
                             Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
                             startActivity(mainIntent);
                             finish();
