9652ec4ed331329ca66e28d9807bca1eb662aa81
Guest User <Guest@epicodus-3.local>
2017-09-21 10:03:31 -0700

    app will login and logout and register fully.

---
+++ app/src/main/java/com/example/guest/dunlapchat/LoginActivity.java
@@ -0,0 +1,108 @@
+package com.example.guest.dunlapchat;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Guest on 9/21/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+
+    private Toolbar mToolbar;
+
+    private TextInputLayout mLoginEmail;
+    private TextInputLayout mLoginPassword;
+
+    private Button mLoginBtn;
+
+    private ProgressDialog mLoginProgress;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = (Toolbar) findViewById(R.id.login_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Login");
+
+        mLoginEmail = (TextInputLayout) findViewById(R.id.login_email);
+        mLoginPassword = (TextInputLayout) findViewById(R.id.login_password);
+        mLoginBtn = (Button) findViewById(R.id.login_button);
+
+        mLoginProgress = new ProgressDialog(this);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = mLoginEmail.getEditText().getText().toString();
+                String password = mLoginPassword.getEditText().getText().toString();
+
+                if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+                    mLoginProgress.setTitle("Logging In");
+                    mLoginProgress.setMessage("Please wait...");
+                    mLoginProgress.setCanceledOnTouchOutside(false);
+                    mLoginProgress.show();
+
+                    loginUser(email, password);
+                }
+            }
+        });
+
+    }
+
+    private void loginUser(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+
+                            mLoginProgress.hide();
+                            Toast.makeText(LoginActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            mLoginProgress.dismiss();
+
+                            Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
--- app/src/main/java/com/example/guest/dunlapchat/StartActivity.java
+++ app/src/main/java/com/example/guest/dunlapchat/StartActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class StartActivity extends AppCompatActivity {
 
     private Button mRegBtn;
+    private Button mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +17,7 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         mRegBtn = (Button) findViewById(R.id.start_reg_button);
+        mLoginBtn = (Button) findViewById(R.id.start_login_btn);
 
         mRegBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +25,15 @@ public class StartActivity extends AppCompatActivity {
 
                 Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
                 startActivity(reg_intent);
+            }
+        });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent login_intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(login_intent);
             }
         });
     }
