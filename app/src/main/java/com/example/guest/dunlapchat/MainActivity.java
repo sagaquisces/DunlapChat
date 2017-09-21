d2ad9420e43053ba7e4e9bf32acf60b79196f887
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-21 15:13:14 -0700

    app will pull display name and status for settings activity

--- app/src/main/java/com/example/guest/dunlapchat/MainActivity.java
+++ app/src/main/java/com/example/guest/dunlapchat/MainActivity.java
@@ -71,6 +71,11 @@ public class MainActivity extends AppCompatActivity {
             sendToStart();
         }
 
+        if(item.getItemId() == R.id.main_settings_btn){
+            Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(settingsIntent);
+        }
+
         return true;
 
 
